48c51c493305666e73bf17b52e6a723a7e5234ef
namanmishra86 <namanmishra8650@gmail.com>
2025-10-08 22:03:11 +0530

    first commit

---
+++ prime.java
@@ -0,0 +1,24 @@
+public class prime {
+    public static boolean isprime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 8; // Example number
+        if (isprime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
+
+
